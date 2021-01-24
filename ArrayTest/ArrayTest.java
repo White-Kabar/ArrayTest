cbaee4a2716b91f9e2f8e7ae9061e8880fa40ca1
Abdullahi Bashiru B. <75587876+White-Kabar@users.noreply.github.com>
2021-01-23 23:24:26 -0800

    Update

---
+++ ArrayTest/ArrayTest.java
@@ -0,0 +1,33 @@
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] fred; int n = 4;
+		fred = new int [6];
+		fred [0] = 2;
+		double [] ted = new double [8];
+		String [] kol = new String [9*3*n];
+		String [] lol = new String [9*90000*n];
+		char [] fog = new char [9];
+		fog [8] = '7';
+		double h = fog[8];
+		System.out.println(lol.length);
+
+	int [] a = new int[5]; 
+	for(int i=0; i<a.length; i++){ 
+	System.out.print(a[i]+"\t"); 
+	} 
+	System.out.print("\n"); 
+	a[0] = 9; 
+	a[1] = 10; 
+	a[2] = a[0]+a[1]; 
+	a[3] = 20; 
+	a[4] = a[3]++; 
+	for(int i=0; i<a.length; i++){ 
+	System.out.print(a[i]+"\t"); 
+	} 
+	} 
+	}
+
+
